96ee05c3fd0e8446621e664f741826839c11f554
Ribka Haryuni <ribka.blue@gmail.com>
2018-10-31 13:32:06 +0700

    directly change string into array with 1 loop

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 function stringArray(str){
-    let result = []
     let newStr = str.split(",")
     for(let i = 0; i < newStr.length; i++){
-        let arr = []
-        let string = newStr[i].split("")
-        let limit = newStr[i].length
-        for(let j = 0; j < limit; j++){
-            arr.push(string[j])
-        }
-        result.push(arr)
+        newStr[i] = newStr[i].split("")
     }
-    return result
+    return newStr
 }
 
 console.log(stringArray("aqrst,ukaei,ffooo"))
